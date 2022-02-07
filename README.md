b57d4b4ff5172ff87664275b469990928b27e496
EtienneV <formation.etienne.re@gmail.com>
2022-02-07 13:24:52 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# spsijesuisriche
+
+Flutter - Projet 1 - Je suis riche
+
+## Getting Started
+
+Projet 1 : introduction développement flutter
---
+++ android/app/src/main/kotlin/re/etienne/jesuisriche/spsijesuisriche/MainActivity.kt
@@ -0,0 +1,6 @@
+package re.etienne.jesuisriche.spsijesuisriche
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
